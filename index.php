d311d467f8da4dcfc2beb7ba7a8949c41eeb4979
MattiaMessana <mattia.messana@icloud.com>
2024-05-22 01:19:38 +0200

    add: check-list and star count

--- index.php
+++ index.php
@@ -65,6 +65,10 @@ $hotels = [
 //     echo "</div>";
 // }
 
+// $si = $_GET['si'];
+// $no = $_GET['no'];
+
+
 ?>
 
 <!DOCTYPE html>
@@ -76,47 +80,71 @@ $hotels = [
     <title>Document</title>
     <!-- boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- style-->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <!-- <?php foreach ($hotels as $list_hotel => $cur_hotel) { ?>
-        <div>
-            <h3><?php echo $cur_hotel['name'] ?></h3>
-            <ul>
-                <li> descrizione: <?php echo $cur_hotel['description'] ?></li>
-                <li> percheggio: <?php echo $cur_hotel['parking'] ?></li>
-                <li> distanza dal centro: <?php echo $cur_hotel['distance_to_center'] ?> km</li>
-                <li> stelle: <?php echo $cur_hotel['vote'] ?></li>
-            </ul>
-        </div>
-    <?php } ?> -->
+    <form action="./parking.php" method="get">
+        <select id="">
+            <option value=""> -- cerca per parcheggio-- </option>
+            <option name="si">si</option>
+            <option name="no">no</option>
+        </select>
+    </form>
 
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Decrizione</th>
-      <th scope="col">Parcheggio</th>
-      <th scope="col">Distanza dal centro</th>
-      <th scope="col">Stelle</th>
-    </tr>
-  </thead>
-  <?php foreach ($hotels as $list_hotel => $cur_hotel) { ?>
-    <tbody>
-        <tr>
-        <th scope="row"><?php echo ($list_hotel += 1); ?></th>
-        <td><?php echo $cur_hotel['name'];?></td>
-        <td><?php echo $cur_hotel['description'];?></td>
-        <td><?php echo $cur_hotel['parking'];?></td>
-        <td><?php echo $cur_hotel['distance_to_center'];?> km</td>
-        <td><?php echo $cur_hotel['vote'];?></td>
-        </tr>
-    </tbody>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Decrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Distanza dal centro</th>
+                <th scope="col">Stelle</th>
+            </tr>
+        </thead>
+        <?php foreach ($hotels as $list_hotel => $cur_hotel) { ?>
+            <tbody>
+                <tr>
+                    <th scope="row"><?php echo ($list_hotel += 1); ?></th>
+                    <td><?php echo $cur_hotel['name']; ?></td>
+                    <td><?php echo $cur_hotel['description']; ?></td>
+                    <td>
+                        <?php
+                            if ($cur_hotel['parking'] === true) {
+                                echo $cur_hotel['parking'] = '<i class="fa-solid fa-check text-bg-success rounded-circle  p"></i>';
+                            } else {
+                                echo $cur_hotel['parking'] = '<i class="fa-solid fa-xmark text-bg-danger rounded-circle px-1 _my_p"></i>';
+                            }
+                        ?>
+                    </td>
+                    <td><?php echo $cur_hotel['distance_to_center']; ?> km</td>
+                    <td>
+                        <!-- stelle -->
+                        <?php
+                            for ($i = 0; $i < $cur_hotel['vote']; $i++) {
+                                $cur_star = $cur_hotel['vote'];
+                                echo $cur_star = '<i class="fa-solid fa-star text-warning"></i>';
+                            }
+                        ?>
+                        <?php
+                            for ($i = 0; $i <  5 - $cur_hotel['vote']; $i++) {
+                                $cur_star = $cur_hotel['vote'];
+                                echo $cur_star = '<i class="fa-regular fa-star"></i>';
+                            }
+                        ?>
+                        <!-- /stelle -->
+                    </td>
+                </tr>
+            </tbody>
+        <?php } ?>
+    </table>
 
-  <?php } ?>
 
-</table>
+
 </body>
 
 </html>
